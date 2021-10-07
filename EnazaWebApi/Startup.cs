3ac56021dededcfe8193e8d8fb2060b977ab0f4c
Katuha-ha <katy_04_94@mail.ru>
2021-10-07 23:47:22 +0300

    Добавлен swagger

--- EnazaWebApi/Startup.cs
+++ EnazaWebApi/Startup.cs
@@ -2,12 +2,11 @@ using EnazaWebApi.Application;
 using EnazaWebApi.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace EnazaWebApi
 {
@@ -24,9 +23,22 @@ namespace EnazaWebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddDbContext<UserContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("Connection")));
+            services.AddDbContext<UserContext>();
             services.AddAutoMapper(c => c.AddProfile<UserProfile>());
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Enaza Test API",
+                    Description = "A simple example ASP.NET Core Web API",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Ekaterina Makarova",
+                        Email = string.Empty,
+                    }
+            });
+        });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,12 +48,17 @@ namespace EnazaWebApi
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseSwagger();
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
 
             app.UseAuthorization();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseEndpoints(endpoints =>
             {
